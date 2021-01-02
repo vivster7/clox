dfaefa55eda8aa58c79ca5319f27f6256cb6e778
Vivek Dasari <vivek.dasari@benchling.com>
2021-01-02 16:29:07 -0500

    17.5 + 17.6 Parsing Infix Expressions + A Pratt Parser

--- compiler.c
+++ compiler.c
@@ -26,6 +26,14 @@ typedef enum {
   PREC_PRIMARY
 } Precedence;
 
+typedef void (*ParseFn)();
+
+typedef struct {
+  ParseFn prefix;
+  ParseFn infix;
+  Precedence precedence;
+} ParseRule;
+
 Parser parser;
 
 Chunk* compilingChunk;
@@ -113,8 +121,49 @@ static void endCompiler() {
   emitReturn();
 }
 
+ParseRule rules[];  // anamoly
+static void expression();
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
+
+static void binary() {
+  // Remember the operator.
+  TokenType operatorType = parser.previous.type;
+
+  // Compile the right operand.
+  ParseRule* rule = getRule(operatorType);
+  parsePrecedence((Precedence)(rule->precedence + 1));
+
+  // Emit the operator instruction.
+  switch (operatorType) {
+  case TOKEN_PLUS:          emitByte(OP_ADD); break;
+  case TOKEN_MINUS:         emitByte(OP_SUBTRACT); break;
+  case TOKEN_STAR:          emitByte(OP_MULTIPLY); break;
+  case TOKEN_SLASH:         emitByte(OP_DIVIDE); break;
+  default:
+    return; // Unreachable.
+  }
+}
+
 static void parsePrecedence(Precedence precedence) {
-  // What goes here?
+  advance();
+  ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+  if (prefixRule == NULL) {
+    error("Expect expression.");
+    return;
+  }
+
+  prefixRule();
+
+  while (precedence <= getRule(parser.current.type)->precedence) {
+    advance();
+    ParseFn infixRule = getRule(parser.previous.type)->infix;
+    infixRule();
+  }
+}
+
+static ParseRule* getRule(TokenType type) {
+  return &rules[type];
 }
 
 static void expression() {
@@ -141,13 +190,55 @@ static void unary() {
   // Compile the operand.
   parsePrecedence(PREC_UNARY);
 
-  switch (operatorType)
-  {
+  switch (operatorType) {
   case TOKEN_MINUS: emitByte(OP_NEGATE); break;
   default:
     return; // Unreachable.
   }
 }
+
+ParseRule rules[] = {
+  [TOKEN_LEFT_PAREN] = {grouping, NULL,   PREC_NONE},
+  [TOKEN_RIGHT_PAREN] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LEFT_BRACE] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_RIGHT_BRACE] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_COMMA] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_DOT] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_MINUS] = {unary,    binary, PREC_TERM},
+  [TOKEN_PLUS] = {NULL,     binary, PREC_TERM},
+  [TOKEN_SEMICOLON] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_SLASH] = {NULL,     binary, PREC_FACTOR},
+  [TOKEN_STAR] = {NULL,     binary, PREC_FACTOR},
+  [TOKEN_BANG] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_BANG_EQUAL] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EQUAL] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EQUAL_EQUAL] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_GREATER] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_GREATER_EQUAL] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LESS] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LESS_EQUAL] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_IDENTIFIER] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_STRING] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_NUMBER] = {number,   NULL,   PREC_NONE},
+  [TOKEN_AND] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_CLASS] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_ELSE] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FALSE] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FOR] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FUN] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_IF] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_NIL] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_OR] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_PRINT] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_RETURN] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_SUPER] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_THIS] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_TRUE] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_VAR] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_WHILE] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_ERROR] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EOF] = {NULL,     NULL,   PREC_NONE},
+};
 
 bool compile(const char* source, Chunk* chunk) {
   initScanner(source);
