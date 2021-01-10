017848df060c0910bb551de3971aeab3fb14038d
Vivek Dasari <vivek.dasari@benchling.com>
2021-01-10 11:08:57 -0500

    18.4 Two New Types

--- vm.c
+++ vm.c
@@ -43,6 +43,10 @@ static Value peek(int distance) {
   return vm.stackTop[-1 - distance];
 }
 
+static bool isFalsey(Value value) {
+  return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
@@ -71,15 +75,32 @@ static InterpretResult run() {
 
     uint8_t instruction;
     Value constant;
+    Value a, b;
     switch (instruction = READ_BYTE()) {
     case OP_CONSTANT:
       constant = READ_CONSTANT();
       push(constant);
       break;
-      case OP_ADD:      BINARY_OP(NUMBER_VAL, +); break;
-      case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
-      case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
-      case OP_DIVIDE:   BINARY_OP(NUMBER_VAL, /); break;
+    case OP_NIL: push(NIL_VAL); break;
+    case OP_TRUE: push(BOOL_VAL(true)); break;
+    case OP_FALSE: push(BOOL_VAL(false)); break;      
+
+    case OP_EQUAL:
+      b = pop();
+      a = pop();
+      push(BOOL_VAL(valuesEqual(a,b)));
+      break;
+    case OP_GREATER:  BINARY_OP(BOOL_VAL, >); break;
+    case OP_LESS:  BINARY_OP(BOOL_VAL, <); break;
+
+    case OP_ADD:      BINARY_OP(NUMBER_VAL, +); break;
+    case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -); break;
+    case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *); break;
+    case OP_DIVIDE:   BINARY_OP(NUMBER_VAL, /); break;
+    case OP_NOT:
+      push(BOOL_VAL(isFalsey(pop())));
+      break;
+    
     case OP_NEGATE: 
       if (!IS_NUMBER(peek(0))) {
         runtimeError("Operand must be a number.");
